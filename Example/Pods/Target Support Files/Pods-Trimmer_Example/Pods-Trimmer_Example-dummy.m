69428d920c175c26af35ef054bbb7a14d12c9233
Diego Caroli <diegocaroli+dtt@gmail.com>
2018-09-25 12:35:38 +0200

    first commit

---
+++ Example/Pods/Target Support Files/Pods-Trimmer_Example/Pods-Trimmer_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Trimmer_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Trimmer_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-Trimmer_Tests/Pods-Trimmer_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Trimmer_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Trimmer_Tests
+@end
