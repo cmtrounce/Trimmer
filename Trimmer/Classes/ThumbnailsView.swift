a6cc114ff31a03d549bb3961604c1fee23b67770
Diego Caroli <diegocaroli+dtt@gmail.com>
2018-11-15 12:47:03 +0100

    Fix bug generate thumbnails.

--- Trimmer/Classes/ThumbnailsView.swift
+++ Trimmer/Classes/ThumbnailsView.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 
 class ThumbnailsView: UIView {
-    
+
     // MARK: Properties
     private let thumbsStackView: UIStackView = {
         let stackView = UIStackView()
@@ -18,61 +18,31 @@ class ThumbnailsView: UIView {
         stackView.distribution = .fillEqually
         return stackView
     }()
-    
+
     open var asset: AVAsset! {
         didSet {
             self.setNeedsLayout()
         }
     }
-    
-    private lazy var assetImageGenerator: AVAssetImageGenerator = {
-        let generator = AVAssetImageGenerator(asset: asset)
-        generator.appliesPreferredTrackTransform = true
-        generator.requestedTimeToleranceAfter = CMTime.zero
-        generator.requestedTimeToleranceBefore = CMTime.zero
-        generator.appliesPreferredTrackTransform = true
-        
-        let scale = UIScreen.main.nativeScale
-        let generatorSize = CGSize(
-            width: thumbnailSize.width * scale,
-            height: thumbnailSize.height * scale)
-        
-        generator.maximumSize = generatorSize
-        
-        return generator
-    }()
-    
-    private lazy var thumbnailSize: CGSize = {
-        guard let track = asset.tracks(withMediaType: AVMediaType.video).first
-            else { fatalError() }
-        
-        let targetSize = bounds.size
-        let assetSize = track.naturalSize.applying(track.preferredTransform)
-        
-        assert(targetSize.width > 0)
-        assert(targetSize.height > 0)
-        
-        let scaleFactor = targetSize.height / assetSize.height
-        let newWidth = assetSize.width * scaleFactor
-        
-        return CGSize(width: newWidth,
-                      height: assetSize.height)
-    }()
-    
+
+    private lazy var assetImageGenerator: AVAssetImageGenerator = makeAssetGenerator(from: asset, withThumbnailSize: thumbnailSize)
+
+    private lazy var thumbnailSize: CGSize = getThumbnailSize(from: asset, with: bounds)
+
     private var totalTimeLength: Int {
         return Int(videoDuration.seconds * Double(videoDuration.timescale))
     }
-    
+
     /// Return the duration of the video
     var videoDuration: CMTime {
         return asset.duration
     }
-    
+
     /// Return the width size that contains the thumbnails
     var durationSize: CGFloat {
         return bounds.width
     }
-    
+
     /// Return the number of thumbnails that will be genearate
     open var currentThumbnailsCount: Int {
         guard asset != nil else { return 0 }
@@ -80,80 +50,90 @@ class ThumbnailsView: UIView {
         number.round(.toNearestOrAwayFromZero)
         return abs(Int(number))
     }
-    
+
     var lastThumbnailsCount: Int = 0 {
         didSet {
-            if lastThumbnailsCount != oldValue {
+            if lastThumbnailsCount != oldValue,
+                lastThumbnailsCount != 0 {
                 regenerateThumbViews(count: lastThumbnailsCount)
                 generateThumbnails()
             }
         }
     }
-    
+
     /// Return the length of each step of the video
     private var videoStep: Int {
         return totalTimeLength / currentThumbnailsCount
     }
-    
+
     // MARK: Inits
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
+
     // MARK: View Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
         setup()
     }
-    
+
     override func didMoveToSuperview() {
         super.didMoveToSuperview()
         self.setNeedsLayout()
     }
-    
-    
+
+
     override func layoutSubviews() {
         super.layoutSubviews()
-        
+
         thumbsStackView.frame = bounds
         lastThumbnailsCount = currentThumbnailsCount
     }
-    
+
     // MARK: Methods
     private func setup() {
         addSubview(thumbsStackView)
+        resetAsset()
     }
-    
+
+    func resetAsset() {
+        guard bounds.width != 0 else { return }
+        self.lastThumbnailsCount = 0
+        self.thumbnailSize = getThumbnailSize(from: asset, with: bounds)
+        self.assetImageGenerator = makeAssetGenerator(
+            from: asset, withThumbnailSize: thumbnailSize)
+    }
+
     /// Generate the thumbnail for each image view
     private func generateThumbnails() {
         assetImageGenerator.cancelAllCGImageGeneration()
-        
+
         let frameForTimes: [NSValue] = (0..<currentThumbnailsCount).map {
             let cmTime = CMTime(value: Int64($0 * videoStep),
                                 timescale: Int32(videoDuration.timescale))
             return NSValue(time: cmTime)
         }
-        
+
         assert(frameForTimes.count == thumbsStackView.arrangedSubviews.count)
-        
+
         DispatchQueue.global(qos: .userInitiated).async { [assetImageGenerator] in
             var index = 0
-            
+
             assetImageGenerator.generateCGImagesAsynchronously(
             forTimes: frameForTimes) { (time, image, _, _, error) in
-                
+
                 guard error == nil else {
                     print("\nError = \(error!)\n")
                     return
                 }
-                
+
                 guard let image = image else { return }
-                
+
                 DispatchQueue.main.async { [weak self] in
                     guard let imageViews = self?.thumbsStackView
                         .arrangedSubviews as? [UIImageView] else { return }
@@ -163,37 +143,37 @@ class ThumbnailsView: UIView {
             }
         }
     }
-    
+
     /// Return the video time from a position of a view
     func getTime(from position: CGFloat) -> CMTime? {
         let normalizedRatio = getNormalizedPosition(from: position)
-        
+
         let positionTimeValue = Double(normalizedRatio)
             * Double(asset.duration.value)
-        
+
         return CMTime(
             value: Int64(positionTimeValue),
             timescale: asset.duration.timescale)
     }
-    
+
     /// Normalized time
     func getNormalizedTime(from time: CMTime) -> CGFloat? {
         let result = CGFloat(time.seconds / asset.duration.seconds)
         assert(result < 1.05)
         return result
     }
-    
+
     /// Return the the position of a view from the video time
     func getPosition(from time: CMTime) -> CGFloat? {
         return getNormalizedTime(from: time)
             .map { $0 * durationSize }
     }
-    
+
     /// Normalized position
     func getNormalizedPosition(from position: CGFloat) -> CGFloat {
         return max(min(1, position / durationSize), 0)
     }
-    
+
     /// Delete the old thumbnails
     func regenerateThumbViews(count: Int) {
         thumbsStackView.arrangedSubviews
@@ -201,7 +181,7 @@ class ThumbnailsView: UIView {
                 thumbsStackView.removeArrangedSubview(view)
                 view.removeFromSuperview()
         }
-        
+
         (0..<count).map { _ in
             let imageView = UIImageView()
             imageView.frame.size = thumbnailSize
@@ -210,3 +190,44 @@ class ThumbnailsView: UIView {
             }.forEach(thumbsStackView.addArrangedSubview)
     }
 }
+
+fileprivate func makeAssetGenerator(
+    from asset: AVAsset,
+    withThumbnailSize thumbnailSize: CGSize) -> AVAssetImageGenerator {
+
+    let generator = AVAssetImageGenerator(asset: asset)
+    generator.appliesPreferredTrackTransform = true
+    generator.requestedTimeToleranceAfter = CMTime.zero
+    generator.requestedTimeToleranceBefore = CMTime.zero
+    generator.appliesPreferredTrackTransform = true
+
+    let scale = UIScreen.main.nativeScale
+    let generatorSize = CGSize(
+        width: thumbnailSize.width * scale,
+        height: thumbnailSize.height * scale)
+
+    generator.maximumSize = generatorSize
+
+    return generator
+}
+
+fileprivate func getThumbnailSize(
+    from asset: AVAsset,
+    with bounds: CGRect) -> CGSize {
+
+    guard let track = asset.tracks(withMediaType: AVMediaType.video).first
+        else { fatalError() }
+
+    let targetSize = bounds.size
+    let assetSize = track.naturalSize.applying(track.preferredTransform)
+
+    assert(targetSize.width > 0)
+    assert(targetSize.height > 0)
+
+    let scaleFactor = targetSize.height / assetSize.height
+    let newWidth = assetSize.width * scaleFactor
+
+    return CGSize(width: newWidth,
+                  height: assetSize.height)
+}
+
--- Trimmer/Classes/TrimmingController.swift
+++ Trimmer/Classes/TrimmingController.swift
@@ -10,14 +10,14 @@ import UIKit
 import AVFoundation
 
 open class TrimmingController: NSObject {
-    
+
     // MARK: IBInspectable
     /// This boolean changes the update frequency of the preview view while scrubbing. If `false`, the scrubbing will only show the frames indicated as "keyframes" (depending on the file codec), if `true`, it will show every frame, at expense of a higher power consumption and worse performance.
     @IBInspectable open var isTimePrecisionInfinity: Bool = false
-    
+
     @IBInspectable open var playImage: UIImage?
     @IBInspectable open var pauseImage: UIImage?
-    
+
     // MARK: IBOutlets
     @IBOutlet open var playPauseButton: UIButton!
     @IBOutlet open var trimmerView: TrimmerView!{
@@ -40,8 +40,6 @@ open class TrimmingController: NSObject {
         return isTimePrecisionInfinity ? .indefinite : .zero
     }
 
-
-    
     // MARK: IBActions
     @IBAction open func playPauseButtonPressed() {
         if !isPlaying {
@@ -51,7 +49,7 @@ open class TrimmingController: NSObject {
             if pauseImage != nil {
                 playPauseButton.setBackgroundImage(
                     pauseImage!.withRenderingMode(.alwaysOriginal),
-                for: .normal)
+                    for: .normal)
             } else {
                 playPauseButton.setTitle("Pause", for: .normal)
             }
@@ -73,7 +71,7 @@ open class TrimmingController: NSObject {
             isPlaying = false
         }
     }
-    
+
     // MARK: Methods
     open func setupPlayerLayer(for url: URL, with playerView: VideoPreviewView) {
         player = AVPlayer(url: url)
@@ -84,20 +82,21 @@ open class TrimmingController: NSObject {
         playerView.setPlayer(player!)
         playerView.addSubview(playPauseButton)
     }
-    
+
     open func setup(asset: AVAsset) {
 
         self.currentStartTime = CMTime.zero
         self.currentEndTime = asset.duration
 
         trimmerView.thumbnailsView.asset = asset
+        trimmerView.thumbnailsView.resetAsset()
     }
-    
+
     /// When the video is finish reset the pointer at the beginning
     private func pause() {
         player?.pause()
         stopPlaybackTimeChecker()
-        
+
         if playImage != nil {
             playPauseButton.setBackgroundImage(
                 playImage!.withRenderingMode(.alwaysOriginal),
@@ -108,7 +107,7 @@ open class TrimmingController: NSObject {
 
         isPlaying = false
     }
-    
+
     /// Schedule a timer
     private func startPlaybackTimeChecker() {
         stopPlaybackTimeChecker()
@@ -117,13 +116,13 @@ open class TrimmingController: NSObject {
             target: self,
             selector: #selector(onPlaybackTimeChecker), userInfo: nil, repeats: true)
     }
-    
+
     /// Invalidate a timer
     func stopPlaybackTimeChecker() {
         playbackTimeCheckerTimer?.invalidate()
         playbackTimeCheckerTimer = nil
     }
-    
+
     /// Update the pointer position respects the current video time
     @objc func onPlaybackTimeChecker() {
         guard let startTime = trimmerView.startTime,
@@ -131,10 +130,10 @@ open class TrimmingController: NSObject {
             let player = player else {
                 return
         }
-        
+
         let playBackTime = player.currentTime()
         trimmerView.seek(to: playBackTime)
-        
+
         if playBackTime >= endTime {
             /// Leave this seek with tolerance zero otherwise will be a little delay of the update of pointer position
             player.seek(to: startTime,
@@ -145,21 +144,21 @@ open class TrimmingController: NSObject {
             trimmerView.resetTimePointer()
         }
     }
-    
+
 }
 
 //MARK: TrimmerViewDelegate
 extension TrimmingController: TrimmerViewDelegate {
-    
+
     public func trimmerDidBeginDragging(_ trimmer: TrimmerView,
                                         with currentTimeTrim: CMTime) {
-        
+
         assert(currentTimeTrim.seconds >= 0)
-        
+
         pause()
         playPauseButton.isHidden = true
     }
-    
+
     public func trimmerDidChangeDraggingPosition(
         _ trimmer: TrimmerView,
         with currentTimePointer: CMTime) {
@@ -171,47 +170,47 @@ extension TrimmingController: TrimmerViewDelegate {
             toleranceBefore: tolerance,
             toleranceAfter: tolerance)
     }
-    
+
     public func trimmerDidEndDragging(
         _ trimmer: TrimmerView,
         with startTime: CMTime,
         endTime: CMTime) {
-        
+
         playPauseButton.isHidden = false
-        
+
         player?.seek(
             to: startTime,
             toleranceBefore: tolerance,
             toleranceAfter: tolerance)
-        
+
         assert(startTime.seconds >= 0)
-        
+
         assert(startTime.seconds <= trimmerView.thumbnailsView.asset.duration.seconds)
-        
+
         assert(endTime.seconds >= 0)
-        
+
         assert(endTime.seconds <= trimmerView.thumbnailsView.asset.duration.seconds)
 
         self.currentStartTime = startTime
         self.currentEndTime = endTime
     }
-    
+
     public func trimmerScrubbingDidBegin(_ trimmer: TrimmerView,
                                          with currentTimeScrub: CMTime) {
         playPauseButton.isHidden = true
-        
+
         assert(currentTimeScrub.seconds >= 0)
     }
-    
+
     public func trimmerScrubbingDidChange(_ trimmer: TrimmerView,
-                                         with currentTimeScrub: CMTime) {
-       
+                                          with currentTimeScrub: CMTime) {
+
         player?.seek(
             to: currentTimeScrub,
             toleranceBefore: tolerance,
             toleranceAfter: tolerance)
         trimmerView.seek(to: currentTimeScrub)
-        
+
         assert(currentTimeScrub.seconds >= 0)
     }
 
@@ -226,6 +225,6 @@ extension TrimmingController: TrimmerViewDelegate {
 
         assert(currentTimeScrub.seconds >= 0)
     }
-    
+
 }
 
